3189ee82d8c83428c84e8a84ede6256732047b0a
alexgoit <alexandrpuryaev@gmail.com>
2023-03-21 16:39:11 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,12 @@
 import Swiper from 'swiper/bundle';
 
 const reviewSwiper = new Swiper('.review-swiper', {
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
 
   // If we need pagination
   pagination: {
@@ -12,26 +18,30 @@ const reviewSwiper = new Swiper('.review-swiper', {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 8
+      spaceBetween: 8,
     },
     // when window width is >= 768px
     768: {
       slidesPerView: 2,
-      spaceBetween: 16
+      spaceBetween: 16,
     },
     // when window width is >= 1200px
     1200: {
       slidesPerView: 3,
-      spaceBetween: 28
-    }
+      spaceBetween: 28,
+    },
   },
-
 });
 
 const productsSwiper = new Swiper('.products-swiper', {
-
   spaceBetween: 18,
-  
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
   // If we need pagination
   pagination: {
     el: '.review-swiper-pagination',
@@ -50,7 +60,6 @@ const productsSwiper = new Swiper('.products-swiper', {
     // when window width is >= 1200px
     1200: {
       slidesPerView: 4,
-    }
+    },
   },
-
 });
